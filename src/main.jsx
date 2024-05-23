267c3a7fa2f8addaf4af26e2bd2984e6f0138d99
namai-dev <113511617+namai-dev@users.noreply.github.com>
2024-05-23 14:43:00 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import Header from "./components/Header.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <TonConnectUIProvider manifestUrl="http://192.168.100.9:5173/manifest.json">
+    <TonConnectUIProvider manifestUrl="https://github.com/namai-dev/not_vc/blob/main/manifest.json">
       <Header />
       <App />
     </TonConnectUIProvider>
